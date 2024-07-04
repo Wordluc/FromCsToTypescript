f6bbe5ee7bc1ed9d36885706390beb2cb0569830
Wordluc <40872910+Wordluc@users.noreply.github.com>
2024-07-04 16:48:09 +0200

    fix: use tcp

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"GoFromCsToTypescript/Writer"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net"
 	"os"
 )
@@ -17,34 +18,65 @@ type Status struct {
 	Msg  string
 }
 
-func sendErrorResponse(e error, server net.PacketConn, addr net.Addr) {
+func sendErrorResponse(e error, conn net.Conn) {
 	status := Status{Code: 500, Msg: e.Error()}
 	resp := Response{Status: status}
 	strResp, _ := json.Marshal(resp)
-	server.WriteTo(strResp, addr)
+	conn.Write(strResp)
 }
+
 func main() {
 	port := os.Args[1]
-	server, _ := net.ListenPacket("udp", "127.0.0.1:"+port)
+	server, err := net.Listen("tcp", "127.0.0.1:"+port)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
 	defer server.Close()
+	fmt.Println("Server listening on port", port)
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+		}
+		result:=make(chan bool)
+		go handleConnection(conn,result)
+		if <-result{
+			break
+		}
+
+	}
+}
+
+func handleConnection(conn net.Conn,result chan bool) {
+	defer conn.Close()
 	str := make([]byte, 10000)
-	resp := Response{}
-	n, addr, e := server.ReadFrom(str)
-	if e != nil {
-		sendErrorResponse(e, server, addr)
-		return ;
+	n, err := conn.Read(str)
+	if err != nil {
+		sendErrorResponse(err, conn)
+		result<-true
+	}
+	if string(str)=="close"{
+		result<-true
 	}
 	if n == 0 {
-		sendErrorResponse(errors.New("no code to convert"), server, addr)
-		return ;
+		sendErrorResponse(errors.New("no code to convert"), conn)
+		result<-true
 	}
-	strConverted, e := Writer.Convert(string(str))
-	if e != nil {
-		sendErrorResponse(e, server, addr)
-		return ;
+	strConverted, err := Writer.Convert(string(str[:n]))
+	if err != nil {
+		sendErrorResponse(err, conn)
+		result<-true
 	}
-	resp.Body = string(strConverted)
-	resp.Status.Code = 200
+	resp := Response{
+		Body: strConverted,
+		Status: Status{
+			Code: 200,
+			Msg:  "Success",
+		},
+	}
 	strResp, _ := json.Marshal(resp)
-	server.WriteTo(strResp, addr)
+	conn.Write(strResp)
+	result<-true
 }
+
